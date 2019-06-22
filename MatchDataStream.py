10f0a9baa65b041cf94b99563b05a1d33a1253f2
Ari Hidayat <49982946+arhieChemEng@users.noreply.github.com>
2019-06-22 22:54:39 +0700

    Delete MatchDataStream.py

--- MatchDataStream.py
+++
@@ -1,8 +0,0 @@
-class MatchData:
-    """MatchData hold information about macthing data"""
-
-    def __init__(self, matchFrom, matchTo, heatLoad, besidePinch):
-        self.matchFrom = matchFrom
-        self.matchTo = matchTo
-        self.heatLoad = heatLoad
-        self.besidePinch = besidePinch
